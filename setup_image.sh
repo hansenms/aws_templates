078292d946e6b31bf194653588e2a4871dd9b8dd
Michael S. Hansen <michael.hansen@nih.gov>
2016-04-26 00:47:12 +0000

    First draft of script to set up image

---
+++ setup_image.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+docker_username=$1
+docker_password=$2
+docker_email=$3
+docker_image=$4
+
+#basic packages
+apt-get update
+apt-get install -y --no-install-recommends git emacs apt-transport-https ca-certificates curl python-pip build-essential automake autotools-dev g++ git libcurl4-gnutls-dev libfuse-dev libssl-dev libxml2-dev make pkg-config jq
+
+#docker 
+sudo apt-key adv --keyserver hkp://p80.pool.sks-keyservers.net:80 --recv-keys 58118E89F3A912897C070ADBF76221572C52609D
+echo "deb https://apt.dockerproject.org/repo ubuntu-trusty main" > /etc/apt/sources.list.d/docker.list
+apt-get update
+apt-get purge lxc-docker
+apt-cache policy docker-engine
+sudo apt-get install -y linux-image-extra-$(uname -r)
+apt-get install -y apparmor
+apt-get install -y docker-engine
+service docker start
+
+#aws cli
+sudo pip install awscli
+
+#s3fs
+git clone https://github.com/s3fs-fuse/s3fs-fuse.git
+cd s3fs-fuse
+./autogen.sh
+./configure --prefix=/usr
+make -j $(nproc)
+make install
+
+#Download Docker image
+docker login -u $docker_username -p $docker_password -e $docker_email
+docker pull $docker_image
+docker tag $docker_image current_gadgetron
