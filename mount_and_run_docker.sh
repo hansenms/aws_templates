c7d6581ef33800d5c0342d08f29ed2c14958980d
Michael S. Hansen <michael.hansen@nih.gov>
2016-05-20 20:23:09 -0400

    Switched to NFS shares

--- mount_and_run_docker.sh
+++ mount_and_run_docker.sh
@@ -1,7 +1,7 @@
-smbhostname=$1
+relayname=$1
 
 mkdir -p /gtmount/gtlog
 mkdir -p /gtmount/gtdependencies 
-echo "//${smbhostname}/gtlog /gtmount/gtlog cifs guest,vers=3.0,dir_mode=0777,file_mode=0777" >> /etc/fstab
-echo "//${smbhostname}/gtdependencies /gtmount/gtdependencies cifs guest,vers=3.0,dir_mode=0777,file_mode=0777" >> /etc/fstab
+echo "${relayhostname}:/home/shares/gtlog /gtmount/gtlog nfs rsize=8192,wsize=8192,timeo=14,intr" >> /etc/fstab
+echo "${relayhostname}:/home/shares/gtdependencies /gtmount/gtdependencies nfs rsize=8192,wsize=8192,timeo=14,intr" >> /etc/fstab
 mount -a
